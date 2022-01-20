7ae9a32471762196557ae2e681796fa5575cc3fd
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-20 20:34:35 +0200

    commit message

---
+++  6. Variables.py
@@ -0,0 +1,18 @@
+x = 4646
+y = 6445
+
+
+
+
+#print(y//x)
+#print(y/x)
+#print(y*x)
+#print(y%x)
+#print(y**x)
+
+Tank_123= 20200
+Tank_1234= 20100
+Tank_22= 5
+Tank_final = Tank_123 + Tank_1234 * Tank_22
+print (Tank_final)
+#Hello_world
