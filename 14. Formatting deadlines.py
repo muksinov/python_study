1143891fd0288e28039647c7ded51e1648783e36
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-22 21:05:52 +0200

    commit message

---
+++ 14. Formatting deadlines.py
@@ -0,0 +1,41 @@
+#print('1'+ '1')
+#Tank = 40
+#print('Tank is ' + str(Tank) + ' years old!')
+
+
+
+#afvs = 1324
+#sfs = 556
+#afvs_and_sfs = 'My name is {0}  I`m {1} years old'.format(afvs,sfs)
+#print(afvs_and_sfs)
+#afvs_and_sfs = 'My name is {0}  I`m {1} years old'.format(1324,556)
+#print(afvs_and_sfs)
+#afvs_and_sfs = 'My name is {}  I`m {} years old'.format(1324,556)
+#print(afvs_and_sfs)
+#weak = 'There are 3 weak: {fri}, {thu}, {mo}.'\
+    #.format(mo='Monday', thu='Thursday', fri='Friday')
+#print(weak)
+
+#float_result = 1000/7
+#print(float_result)
+#print('The result of division is {0:10.3f}'.format(float_result))
+#print('''
+#{0:10.2f}{1:10.2f}{2:10.2f}
+#{3:10.2f}{4:10.2f}{5:10.2f}
+#{6:10.2f}{7:10.2f}{8:10.2f}
+
+#'''.format(1.45778, 345.232352, 34.2344, 1234.23,
+           #1.45778, 345.232352, 34.2344, 1234.23,
+           #456.43234))
+
+name = 'Tank'
+age = 23
+name_and_age = 'My name is {0}  I`m {1} years old'.format(name,age)
+print(name_and_age)
+name_and_age = f'My name is {name}  I`m {age} years old'
+print(name_and_age)
+
+print("My name is %s. %s %d years old" % (name,"I'm", age))
+
+
+
