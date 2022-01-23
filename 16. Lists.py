5da21f3cca171127bfd12e5b7d46d64f1feea86e
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-23 20:01:31 +0200

    commit message

---
+++ 16. Lists.py
@@ -0,0 +1,51 @@
+number_list = [23, 34, 25, 45.56]
+print(number_list)
+qerr = [54, 76, 'Tank']
+print(qerr)
+print(len(qerr))
+print(qerr[1])
+daasf = qerr[:2]
+print(daasf)
+qerr[2] = 'Tak'
+print(qerr)
+fdf = qerr + daasf
+print(fdf)
+#fdf[5] = 'Fordg'
+fdf.append('Fordg')
+fdf.insert(2, 13 )
+
+#fdf.pop(-1)
+#df.pop(0)
+#fdf.pop(-3)
+#st=fdf.pop()
+#st=fdf.pop(-3)
+#st=fdf.pop(12)
+
+
+
+
+
+
+
+
+
+
+
+#print(st)
+#print(fdf)
+
+fdf=[25, 34, 25, 45.56]
+hgh=['t', 'a', 'n', 'k' ]
+x=fdf.sort()
+fdhfh=fdf
+
+
+
+print(x)
+print(fdhfh)
+
+fdf.reverse()
+fdf.reverse()
+fdf.append(fdf)
+print(fdf)
+print(hgh)
