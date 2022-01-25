aac7acb3790d86c0417411db73ad8c2a45a994de
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-25 20:08:19 +0200

    commit message

---
+++ 18. Dictionaries.py
@@ -0,0 +1,20 @@
+zfgr = {'a': 1, 'b': 2, 'd': 3}
+print (zfgr['a'])
+zfgr ['c'] = 4
+print(zfgr)
+zfgr['a']= 2
+print(zfgr)
+del zfgr['d']
+print(zfgr)
+
+
+fg= {
+    'ffsg': 'gtgtg',
+    'jggk': 'gkhkhk',
+    'dfigg': ['dgfdfg', 'dhfhh']
+
+}
+print(fg['ffsg'])
+dfigg = fg['dfigg']
+print(dfigg[1])
+print(fg.keys())
