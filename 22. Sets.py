271d2eb3f8a87c8c604601b3e24e2f8a26ed81b2
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-27 21:31:23 +0200

    commit message

---
+++ 22. Sets.py
@@ -0,0 +1,34 @@
+tghhh ={'a', 'b', 'd', 'c'}
+print(tghhh )
+print(type(tghhh ))
+abdc = set()
+print(abdc)
+print(type(abdc))
+
+A123 =(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,2021)
+abcd = ['hi', 'hello', 'hollo']
+rghghg =set(A123)
+hjhhj=set(abcd)
+
+rghghg.add(467598657807869679087989890907880767670890787689787868754566590765786706976807676457690746487)
+hjhhj.add('si0[hohohg]hmj;kphbkjmlboogi[jhbpk,bkjy'
+          'yuokjmlnfjrvhjglhg;knvnbllnx vvfhbrfh;ohlkjjhljlmljllllcjgfjytjyjjjhdfdfhgtdjsyrj4mjyh,jm')
+rghghg.add(467598657807869679087989890907880767670890787689787868754566590765786706976807676457690746487)
+hjhhj.add('si0[hohohg]hmj;kphbkjmlboogi[jhbpk,bkjy'
+          'yuokjmlnfjrvhjglhg;knvnbllnx vvfhbrfh;ohlkjjhljlmljllllcjgfjytjyjjjhdfdfhgtdjsyrj4mjyh,jm')
+rghghg.pop()
+rghghg.remove(19)
+rghghg.discard(2021)
+rghghg.discard(2021)
+#rghghg.remove(19)
+rghghg.clear()
+
+
+
+
+
+
+print(rghghg)
+print(hjhhj)
+
+
