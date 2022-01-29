3df21891a1821b46e14d59bfbb99c0e8d7b15a1c
mukshynov <oleksii.mukshynov@gmail.com>
2022-01-29 21:02:07 +0200

    commit message

---
+++ 25. Booleans.py
@@ -0,0 +1,45 @@
+#print(1 < 2)
+#print(type(True))
+#print(type(False))
+#print(432453 == 464576868678)
+#print(555 == 555)
+#print('Home' == 'Home')
+#print('home' == 'Home')
+
+
+#print(1 != 1)
+#print(1 != 2)
+#print('Home' != 'Home')
+#print('home' != 'Home')
+
+
+
+#print(1 < 2)
+#print(1 <= 2)
+#print(1 >= 2)
+
+
+
+
+#print(ord('a'))
+#print(ord('b'))
+#print('a'> 'b')
+#print('rsfg' < 'sdff')
+#print(ord('s'))
+#print(ord('d'))
+
+
+#x = 675745675684767976078996
+#y = 63756090568576666745676767.76767697879
+#print(y < x)
+#print(y > x)
+#print(y == x)
+#print(y != x)
+
+
+Tank = int(input('How many Tank'))
+print('Access is permitted:' + str(Tank >= 20))
+
+
+
+
