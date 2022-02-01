65d2888b1b4dc16699c04825b101c65784847804
mukshynov <oleksii.mukshynov@gmail.com>
2022-02-01 21:28:11 +0200

    commit message

---
+++ 27. Logical operators.py
@@ -0,0 +1,23 @@
+#y=1
+#x=2
+
+#print(x > 1 and y < 1)
+#print(x > 1 or y < 1)
+#print(not x > 1)
+#print(not y > 1)
+#print(True and True )
+#print(True or True )
+#print(not True  )
+#print(False and False )
+#print(False or False )
+#print(not False )
+#print(True and False )
+#print(True or False )
+
+
+name = '123'
+age = 41
+is_married=False
+
+if age > 18 or is_married==False:
+    print('Hi {}'.format(name))
---
+++ 28. Условный оператор if elif else.py
@@ -0,0 +1,42 @@
+#x = 4
+
+#if x > 3:
+    #print('x > 3')
+    #print('I`m happy')
+#elif x < 3:
+    #print('x < 3')
+    #print('x == 3')
+
+
+#day_time = 'night'
+
+
+
+#if day_time == 'midnight':
+    #print('Monster wakes up')
+#elif day_time == 'afternoon':
+  # print('Moster is walking')
+#elif day_time == 'evening':
+   # print('Moster is eating')
+#elif day_time == 'night':
+   # print('Monster is sleeping')
+#else:
+   # print('Moster is doing something')
+
+
+   #x = 40
+   #if x % 2 == 0:
+       #print('x is even')
+   #else:
+   #    print('x is odd')
+  # print('Some text')
+
+
+
+  #user_input = input ('Input something ')
+  #if user_input == 'Hello':
+   #print('Hello! Nice to meet you!')
+
+   if [1, 3]:
+       print('Something')
+
