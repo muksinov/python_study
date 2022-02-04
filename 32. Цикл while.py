9981aec80e601f7c4c677fcd7f80b7c8383aa0aa
mukshynov <oleksii.mukshynov@gmail.com>
2022-02-04 21:39:15 +0200

    commit message

---
+++ 32. Цикл while.py
@@ -0,0 +1,75 @@
+#x= 5
+#while x >= 1:
+    #print(x)
+   # x=x -1
+
+#while x < 1001:
+    #print(x)
+    #x = x + 1
+
+#while x < 10:
+    #print(x)
+    #x += 2
+#print('Next code')
+
+
+#x = 0
+#while x < 10:
+   # print(x)
+   # x += 1
+#else:
+   # print('x n 10')
+#for x in range(10):
+    #print('x is less than 10')
+#else:
+   # x+=1
+#    print('x now is not less than 10')
+
+
+my_list = [1, 2, 3]
+
+#for item in my_list:
+    #pass
+#print('Another code')
+
+#
+    # break
+    #print(item)
+
+#print('Another code')
+
+for item in my_list:
+    if item == 2:
+        continue
+    print(item)
+
+print('Another code')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
