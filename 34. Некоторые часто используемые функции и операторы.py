3e17132d06e627c38edfb0c9ef16509892a4b94c
mukshynov <oleksii.mukshynov@gmail.com>
2022-02-05 20:39:52 +0200

    commit message

---
+++ 34. Некоторые часто используемые функции и операторы.py
@@ -0,0 +1,68 @@
+#for x in range (3, 11, 3):
+  #  print(x)
+#print(range(5))
+#print(list(range(5)))
+
+
+#letter_index = 0
+#my_string = 'agdhhtg'
+#for letter in my_string:
+   # print(letter + 'is at index' + str(letter_index))
+
+
+#my_string = 'efhthjhj'
+#for item in enumerate(my_string):
+   # print(item)
+
+
+#print('a' in 'amogus')
+#print('d' in 'amogus')
+#print(str(1) in 'amogus')
+#print('2' in 'amogus')
+
+
+#letter_list = ['a', 'd', 'f', True]
+#print('a' in letter_list)
+#print(True in letter_list)
+
+#dict_1 = {1: 'a', 2: 'b', 3: 'c'}
+#print(1 in dict_1)
+#print(1 in dict_1.keys())
+#print(4 in dict_1.keys())
+#print('c' in dict_1.values())
+#print('j' in dict_1.values())
+
+
+#print(min(1, 3, 56, 4))
+#print(max(1, 3, 56, 4))
+
+#my_list = [1, 3, 56, 4]
+#print(min(my_list))
+#print(max('Hello'))
+#for letter in 'Hello':
+   # print(ord(letter))
+
+from random import shuffle
+my_list = [1, 2, 44, 5]
+shuffle(my_list)
+print(my_list)
+
+from random import randint
+print(randint(1,100000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
