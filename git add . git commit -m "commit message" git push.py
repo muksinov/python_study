ebf7753ede64bcd354a2be1861017bd1ba067eab
Tank1232010 <muksinovalesa@gmail.com>
2022-03-14 22:17:06 +0200

    commit message

---
+++ git add . git commit -m "commit message" git push.py
@@ -0,0 +1,112 @@
+from unittest import result
+
+
+# def sum_of_two_numbers(x):
+#     return x + x
+
+
+# number_list = [1, 2, 3, 4, 5, 6, 7]
+
+
+# result = map(sum_of_two_numbers, number_list)
+# print(result)
+
+# for item in result:
+#     print(item)
+
+# for item in map(sum_of_two_numbers, number_list):
+#     print(item)
+
+# print(list(map(sum_of_two_numbers, number_list)))
+
+
+# def is_a_in_string(string):
+#     if 'a' in string:
+#         print('String has "a"')
+#         return True
+#     else:
+#         print('String has not "a"')
+#         return False
+
+
+
+# string_list = ['hi', 'was', 'name', 'he']
+
+# print(list(map(is_a_in_string('hey'), string_)))
+
+
+
+def is_number_odd(number):
+    return number % 2==1
+
+
+number_list = [1, 2, 3, 4, 5, 6, 7]
+
+print(filter(is_number_odd, number_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
